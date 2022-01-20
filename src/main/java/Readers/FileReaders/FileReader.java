a817b8817ad930e043084f65800c30aee94b7dcd
DanielKhaikin <daniel2003khaikin@gmail.com>
2022-01-20 08:58:44 +0200

    finished abstract class with con, get,set and read

--- src/main/java/Readers/FileReaders/FileReader.java
+++ src/main/java/Readers/FileReaders/FileReader.java
@@ -1,7 +1,24 @@
 package Readers.FileReaders;
 
+import Readers.Reader;
+
 import java.io.File;
 
-public abstract class FileReader {
+public abstract class FileReader implements Reader {
     private File file;
+
+    public FileReader(File file) {
+        this.file = file;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    @Override
+    public abstract void read();
 }
