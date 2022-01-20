3b0c0d0e67f5bb9384a3dec6827c497d8f95439e
DanielKhaikin <daniel2003khaikin@gmail.com>
2022-01-20 09:01:16 +0200

    created class

---
+++ src/main/java/Readers/FileReaders/CsvReader.java
@@ -0,0 +1,14 @@
+package Readers.FileReaders;
+
+import java.io.File;
+
+public class CsvReader extends FileReader{
+    public CsvReader(File file) {
+        super(file);
+    }
+
+    @Override
+    public void read() {
+        
+    }
+}
