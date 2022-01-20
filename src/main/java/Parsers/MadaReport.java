1d5f4ad686a3188788f26ea577e297a87fc8f673
DanielKhaikin <daniel2003khaikin@gmail.com>
2022-01-20 09:36:30 +0200

    finished class with cons,getts and setts

--- src/main/java/Parsers/MadaReport.java
+++ src/main/java/Parsers/MadaReport.java
@@ -1,2 +1,129 @@
-package Parsers;public class MadaReport {
+package Parsers;
+
+import java.util.Date;
+
+public class MadaReport {
+    private String MDACODE;
+    private String IDNum;
+    private int IDType;
+    private String firstName;
+    private String lastName;
+    private String city;
+    private String street;
+    private int buildingNumber;
+    private String barcode;
+    private Date getDate;
+    private Date takeDate;
+    private Date resultDate;
+
+    public MadaReport(String MDACODE, String IDNum, int IDType, String firstName, String lastName, String city, String street, int buildingNumber, String barcode, Date getDate, Date takeDate, Date resultDate) {
+        this.MDACODE = MDACODE;
+        this.IDNum = IDNum;
+        this.IDType = IDType;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.street = street;
+        this.buildingNumber = buildingNumber;
+        this.barcode = barcode;
+        this.getDate = getDate;
+        this.takeDate = takeDate;
+        this.resultDate = resultDate;
+    }
+
+    public String getMDACODE() {
+        return MDACODE;
+    }
+
+    public void setMDACODE(String MDACODE) {
+        this.MDACODE = MDACODE;
+    }
+
+    public String getIDNum() {
+        return IDNum;
+    }
+
+    public void setIDNum(String IDNum) {
+        this.IDNum = IDNum;
+    }
+
+    public int getIDType() {
+        return IDType;
+    }
+
+    public void setIDType(int IDType) {
+        this.IDType = IDType;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getBuildingNumber() {
+        return buildingNumber;
+    }
+
+    public void setBuildingNumber(int buildingNumber) {
+        this.buildingNumber = buildingNumber;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public Date getGetDate() {
+        return getDate;
+    }
+
+    public void setGetDate(Date getDate) {
+        this.getDate = getDate;
+    }
+
+    public Date getTakeDate() {
+        return takeDate;
+    }
+
+    public void setTakeDate(Date takeDate) {
+        this.takeDate = takeDate;
+    }
+
+    public Date getResultDate() {
+        return resultDate;
+    }
+
+    public void setResultDate(Date resultDate) {
+        this.resultDate = resultDate;
+    }
 }
