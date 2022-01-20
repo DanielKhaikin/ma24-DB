b0885511f264772b58c885f402624050a718b301
DanielKhaikin <daniel2003khaikin@gmail.com>
2022-01-20 14:26:44 +0200

    finished MadaReport writer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,23 @@
 import Parsers.MadaReport;
 import Readers.FileReaders.CsvReaders.MadaReportReader;
+import Writers.FileWriters.CsvWriters.MadaReportsWriter;
 
 import java.io.File;
 import java.util.HashSet;
+import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        File file = new File("C:\\Users\\Daniel\\BuildADB\\src\\main\\resources\\MadaReports.csv");
-        MadaReportReader madaReportReader = new MadaReportReader<>(file);
+        File sourceFile = new File("C:\\Users\\Daniel\\BuildADB\\src\\main\\resources\\MadaReports.csv");
+        File destFile = new File("C:\\Users\\Daniel\\HadogemHamatmidJson\\madarep.csv");
+        MadaReportReader madaReportReader = new MadaReportReader<>(sourceFile);
         HashSet<MadaReport> madaReports = (HashSet<MadaReport>) madaReportReader.read();
+        LinkedList<MadaReport> list = new LinkedList<>();
+        for (MadaReport madaReport: madaReports ) {
+            list.add(madaReport);
+        }
+        MadaReportsWriter madaReportsWriter = new MadaReportsWriter<>(destFile);
+        madaReportsWriter.write(list);
         System.out.println(madaReports.size());
     }
 }
--- src/main/java/Writers/FileWriters/CsvWriters/MadaReportsWriter.java
+++ src/main/java/Writers/FileWriters/CsvWriters/MadaReportsWriter.java
@@ -15,18 +15,18 @@ public class MadaReportsWriter<T> extends CsvWriter{
     @Override
     public CsvSchema createTable() {
         CsvSchema schema = CsvSchema.builder().setUseHeader(true)
-                .addColumn("MDACODE")
-                .addColumn("IDNum")
-                .addColumn("IDType")
-                .addColumn("FirstName")
-                .addColumn("LastName")
-                .addColumn("City")
-                .addColumn("Street")
-                .addColumn("BuildingNumber")
-                .addColumn("Barcode")
-                .addColumn("GetDate")
-                .addColumn("TakeDate")
-                .addColumn("ResultDate")
+                .addColumn("mdacode")
+                .addColumn("idnum")
+                .addColumn("idtype")
+                .addColumn("firstName")
+                .addColumn("lastName")
+                .addColumn("city")
+                .addColumn("street")
+                .addColumn("buildingNumber")
+                .addColumn("barcode")
+                .addColumn("getDate")
+                .addColumn("takeDate")
+                .addColumn("resultDate")
                 .build();
         return schema;
     }
